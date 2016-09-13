730e4a312a4c20be80849ded1cebfa450b38ceaf
srhbnnstr <srhbnnstr@gmail.com>
2016-09-13 14:20:34 -0500

    adding resume pdf

--- mailer.php
+++ mailer.php
@@ -18,7 +18,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "srhbnnstr@gmail.com";
+        $recipient = "admin@sarah-bannister.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
